53c39ebe33e9c912774972e735d5785a54e2ec02
ZainShakir <K190178@nu.edu.pk>
2022-06-21 19:02:13 +0500

    with except handling

--- Day2_Task/main.py
+++ Day2_Task/main.py
@@ -2,52 +2,62 @@ import math
 
 
 def PrimeFunction():
-    num = int(input('Enter any Number: '))
-    if num > 1:
-        for i in range(2, int(num / 2) + 1):
-            if (num % i == 0):
-                return print(num, 'is not a prime number')
-                break
+    try:
+        num = int(input('Enter any Number: '))
+        if num > 1:
+            for i in range(2, int(num / 2) + 1):
+                if (num % i == 0):
+                    return print(num, 'is not a prime number')
+                    break
 
+                else:
+                    return print(num, "is a prime number")
+                    break
             else:
-                return print(num, "is a prime number")
-                break
-        else:
-            return print(num, "is not a prime number")
+                return print(num, "is not a prime number")
+    except:
+        print("Wrong Input types")
+
 def DistanceFormula():
-    x1=float(input("Enter the value of x1: "))
-    y1=float(input("Enter the value  of y1: "))
-    x2=float(input("Enter the value of x2: "))
-    y2=float(input("Enter the value of y2: "))
-    distance=math.sqrt(pow(x2-x1,2)+pow(y2-y1,2))
-    print(f"The distacne between the two points is {distance}")
+    try:
+        x1=float(input("Enter the value of x1: "))
+        y1=float(input("Enter the value  of y1: "))
+        x2=float(input("Enter the value of x2: "))
+        y2=float(input("Enter the value of y2: "))
+        distance=math.sqrt(pow(x2-x1,2)+pow(y2-y1,2))
+        print(f"The distacne between the two points is {distance}")
+    except:
+        print("Wrong Input type")
     
 def QuadraticFormula():
-    a = int(input('Enter a:'))  
-    b = int(input('Enter b:'))  
-    c = int(input('Enter c:')) 
-    dis_form = b * b - 4 * a * c  
-    sqrt_val = math.sqrt(abs(dis_form))  
-  
-  
-    if dis_form > 0:  
-        print(" real and different roots ")  
-        print((-b + sqrt_val) / (2 * a))  
-        print((-b - sqrt_val) / (2 * a))  
-  
-    elif dis_form == 0:  
-        print(" real and same roots")  
-        print(-b / (2 * a))  
-  
-  
-    else:  
-        print("Complex Roots")  
-        print(- b / (2 * a), " + i", sqrt_val)  
-        print(- b / (2 * a), " - i", sqrt_val)  
-   
-  
-    if a == 0:  
-        print("Input correct quadratic equation")  
+    try:
+        a = int(input('Enter a:'))  
+        b = int(input('Enter b:'))  
+        c = int(input('Enter c:')) 
+        dis_form = b * b - 4 * a * c  
+        sqrt_val = math.sqrt(abs(dis_form))  
+    
+    
+        if dis_form > 0:  
+            print(" real and different roots ")  
+            print((-b + sqrt_val) / (2 * a))  
+            print((-b - sqrt_val) / (2 * a))  
+    
+        elif dis_form == 0:  
+            print(" real and same roots")  
+            print(-b / (2 * a))  
+    
+    
+        else:  
+            print("Complex Roots")  
+            print(- b / (2 * a), " + i", sqrt_val)  
+            print(- b / (2 * a), " - i", sqrt_val)  
+    
+    
+        if a == 0:  
+            print("Input correct quadratic equation")  
+    except:
+        print("Wrong Input Type")
     
 def Slopeformula():
     try:
@@ -80,11 +90,15 @@ def Slopeformula():
     
 def TrignometricIdentity():
     return "friday"
+
 def sum():
-    x = int(input("Type First number: "))
-    y = int(input("Type Second number: "))
-    sum1 = int(x) + int(y)
-    print("The sum is: ", sum1)
+    try:
+        x = int(input("Type First number: "))
+        y = int(input("Type Second number: "))
+        sum1 = int(x) + int(y)
+        print("The sum is: ", sum1)
+    except:
+        print("Wrong Input Types")
     
 def default():
     return "Invalid choice"
@@ -103,4 +117,4 @@ def switch(formula):
 
 if __name__ == "__main__":
     formula = float(input('Enter choice: '))
-    print(switch(formula))
+    switch(formula)
